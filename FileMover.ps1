f7232abf63133b928eb9db8ea029b8aa6d0fdf8a
Nick Howell <nick@nickhowell.co.uk>
2017-01-09 16:36:13 +0000

    First pass. Moves files according to Y/M/D
    Will take all files in the specified directory and move them to a sub folder structure according to their last modified time

---
+++ FileMover.ps1
@@ -0,0 +1,30 @@
+﻿$dir = 'C:\testing\photos1'
+
+cd $dir
+
+$files = Get-ChildItem -File
+
+foreach ($file in $files)
+{
+    $year = $file.LastWriteTime.Year
+    $month = $file.LastWriteTime.Month
+    $day = $file.LastWriteTime.Day
+    $name = $file.BaseName
+    
+    $destination = "$dir\$year\$month\$day"
+
+    if (Test-Path $destination)
+    {
+        Write-Host "Moving $name to $destination."
+        Move-Item $file $destination
+    }
+    else
+    {
+        Write-Host "Need to create $destination"
+        if(New-Item $destination -ItemType Directory)
+        {
+            Write-Host "Moving $name to $destination"
+            Move-Item $file $destination
+        }
+    }
+}
